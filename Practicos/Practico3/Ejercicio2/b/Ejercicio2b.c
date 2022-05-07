750548e0fd8232bef59b2ede9c865f89572cb9a9
Ignacio Perciante <ignacioperciante.crypto@gmail.com>
2022-05-07 14:54:16 -0300

    Agregando Practicos (organizando)

---
+++ Practicos/Practico3/Ejercicio2/b/Ejercicio2b.c
@@ -0,0 +1,14 @@
+/*
+Escribir, compilar y ejecutar en C algoritmos que den solución a los siguientes problemas. Hacer
+un programa separado para resolver cada uno de ellos:
+b) Desplegar los mensajes “Esta es la primer linea.” y “Esta es la segunda
+linea.” en dos líneas diferentes.
+*/
+
+#include <stdio.h>
+int main()
+{
+     printf("Esta es la primer linea.\n"); //el \n produce el salto de linea
+     printf("Esta es la segunda linea.");
+     return 0;
+}
